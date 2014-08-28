73d3e5e51ec8297a13fc62d441f79f1bdb889f6a
BitMo <bitmocurrency@gmail.com>
2014-08-28 11:05:05 +0200

    Initial commit for the reboot of XBM! Let's do it!

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BitMo-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BitMoURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
